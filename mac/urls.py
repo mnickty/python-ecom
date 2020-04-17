2e928db9a125c17274a165fe1ee1e67e06143dce
Nickty <flipcute.com@gmail.com>
2020-04-17 15:11:05 +0600

    second commit

--- mac/urls.py
+++ mac/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
     path('blog/', include('blog.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
